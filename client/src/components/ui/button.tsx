307648b8043e75f3a9b8695c164736bd87ce18b3
Irem Ecem Yelkanat <iremecemyelkanat@gmail.com>
2025-05-21 23:43:16 +0200

    [version] add not found page

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-9 px-4 py-2',
+        default: 'rounded-lg border-2 px-4 py-2 font-semibold shadow-lg',
         sm: 'h-8 rounded-md px-3 text-xs',
         lg: 'rounded-lg border-2 px-6 py-3 text-lg font-semibold shadow-lg',
         icon: 'size-9',
