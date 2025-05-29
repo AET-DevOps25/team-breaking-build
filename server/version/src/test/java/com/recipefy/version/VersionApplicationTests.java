955a4b75dfd36cc7c89a126aee52cd2b2503c116
İrem Ecem Yelkanat <91906816+iremyelkanat@users.noreply.github.com>
2025-05-29 12:09:16 +0200

    [version] add db config in tests (#33)

--- server/version/src/test/java/com/recipefy/version/VersionApplicationTests.java
+++ server/version/src/test/java/com/recipefy/version/VersionApplicationTests.java
@@ -2,8 +2,10 @@ package com.recipefy.version;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class VersionApplicationTests {
 
 	@Test
