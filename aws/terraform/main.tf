5f478a3e15a1c3fa1aad2ea2a2ece03c9efb2909
Irem Ecem Yelkanat <iremecemyelkanat@gmail.com>
2025-05-29 14:06:09 +0200

    [app] add terraform files

---
+++ aws/terraform/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical Ubuntu
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "recipefy" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Recipefy App"
+  }
+}
