f830f57f148a4e75ee569e9fa4c41787ceed35e0
Erhan Varlik <erhanvarlik1@gmail.com>
2025-07-06 16:40:47 +0200

    fix eof

--- server/version/src/main/java/com/recipefy/version/config/WebConfig.java
+++ server/version/src/main/java/com/recipefy/version/config/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")           // Spring 5.3+ / Boot 2.4+
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
