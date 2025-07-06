c77c17aeff13429996952a1b180d8bb0114e80f1
Erhan Varlik <erhanvarlik1@gmail.com>
2025-07-07 00:19:09 +0200

    [recipe] remove cors configuration

--- server/recipe/src/main/java/com/recipefy/recipe/config/WebConfig.java
+++ server/recipe/src/main/java/com/recipefy/recipe/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.recipefy.recipe.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,15 +19,5 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(requestLoggingInterceptor)
                 .addPathPatterns("/**") // Apply to all paths
                 .excludePathPatterns("/actuator/**"); // Exclude actuator endpoints
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
     }
 }
