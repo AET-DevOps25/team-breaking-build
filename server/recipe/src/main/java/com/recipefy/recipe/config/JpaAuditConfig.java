9461596c64f9ab2a5b80edce15f1ca7e72c77129
Erhan Varlık <erhanvarlik1@gmail.com>
2025-07-08 16:23:56 +0200

    [recipe] enable jpa auditing (#75)

---
+++ server/recipe/src/main/java/com/recipefy/recipe/config/JpaAuditConfig.java
@@ -0,0 +1,9 @@
+package com.recipefy.recipe.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditConfig {
+}
