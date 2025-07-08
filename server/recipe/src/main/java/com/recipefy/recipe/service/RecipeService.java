ad0337fe842a457b28fee9d941a7755528b32755
Erhan Varlik <erhanvarlik1@gmail.com>
2025-07-08 14:33:17 +0200

    [recipe] change get recipe endpoints

--- server/recipe/src/main/java/com/recipefy/recipe/service/RecipeService.java
+++ server/recipe/src/main/java/com/recipefy/recipe/service/RecipeService.java
@@ -12,11 +12,11 @@ import com.recipefy.recipe.model.request.CreateRecipeRequest;
 
 public interface RecipeService {
     Page<RecipeMetadataDTO> getAllRecipes(Pageable pageable);
+    Page<RecipeMetadataDTO> getAllRecipes(UUID userId, Pageable pageable);
     RecipeMetadataDTO getRecipe(Long recipeId);
     RecipeMetadataDTO createRecipe(CreateRecipeRequest request, UUID userId);
     RecipeMetadataDTO updateRecipe(Long recipeId, RecipeMetadataDTO metadataDTO, UUID userId);
     RecipeMetadataDTO copyRecipe(Long recipeId, UUID userId, Long branchId);
     void deleteRecipe(Long recipeId, UUID userId);
-    RecipeMetadataDTO updateTags(Long recipeId, List<RecipeTagDTO> tags, UUID userId);
     List<RecipeTagDTO> getAllTags();
 }
