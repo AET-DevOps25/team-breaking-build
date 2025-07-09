025c5ca0e9b2bb37b28bb747ee27c5626287c4dc
Subfly <alitahasubfly@gmail.com>
2025-07-09 20:19:46 +0200

    [VERSION] Remove cors as gateweay already handles

--- server/version/src/main/java/com/recipefy/version/config/WebConfig.java
+++ server/version/src/main/java/com/recipefy/version/config/WebConfig.java
@@ -1,10 +1,10 @@
 package com.recipefy.version.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * Web configuration to register interceptors
@@ -20,15 +20,5 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(requestLoggingInterceptor)
                 .addPathPatterns("/**") // Apply to all paths
                 .excludePathPatterns("/actuator/**"); // Exclude actuator endpoints
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
     }
 }
