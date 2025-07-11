541e8e9239e3f84bce3c4a9e96b57853733d9d3b
Erhan Varlik <erhanvarlik1@gmail.com>
2025-07-11 16:11:42 +0200

    [client] add rewrite rules

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,6 +4,19 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path',
+        destination: 'http://recipefy-api-gateway:8080/:path',
+      },
+      {
+        source: '/auth/:path',
+        destination: 'http://recipefy-keycloak-service-keycloak-spi:8080/auth/:path',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
